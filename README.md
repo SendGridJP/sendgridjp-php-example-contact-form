f8dd3ed4f7ad699222c009301f8785500a06a034
Daisuke Aoki <daisuke-aoki@kke.co.jp>
2022-05-13 15:38:05 +0900

    update multiple files

--- README.md
+++ README.md
@@ -17,13 +17,11 @@ composer install
 .envファイルは以下のような内容になっています。
 
 ```bash
-SENDGRID_USERNAME=your_username
-SENDGRID_PASSWORD=your_password
+SENDGRID_APIKEY=your_apikey
 TO=you@youremail.com
 FROM=you@youremail.com
 ```
-SENDGRID_USERNAME:SendGridのユーザ名を指定してください。  
-SENDGRID_PASSWORD:SendGridのパスワードを指定してください。  
+SENDGRID_APIKEY:SendGridのAPIキーを指定してください。   
 TO:フォームの内容を送信する宛先を指定してください。  
 FROM:フォームの内容を送信する際のFromアドレスを指定してください。  
 
--- mailer.php
+++ mailer.php
@@ -2,8 +2,7 @@
 require 'vendor/autoload.php';
 Dotenv::load(__DIR__);
 
-$sendgrid_username = $_ENV['SENDGRID_USERNAME'];
-$sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
+$sendgrid_apikey   = $_ENV['SENDGRID_APIKEY'];
 $from              = $_ENV['FROM'];
 $to                = $_ENV['TO'];
 
@@ -12,15 +11,14 @@ $emailadd = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
-$email    = new SendGrid\Email();
-$email->addTo($to)->
-       setFrom($from)->
-       setFromName("問合せフォーム")->
-       setSubject("[ContactForm] $subject")->
-       setText("Name: $name \r\nEmail: $emailadd \r\nSubject: $subject \r\nMessage: $message \r\n")->
-       setHtml("<strong>Name:</strong> $name<br /> <strong>Email:</strong> $emailadd<br /> <strong>Subject:</strong> $subject<br /> <strong>Message:</strong> $message<br /> ")->
-       addCategory('contact');
+$sendgrid = new SendGrid($sendgrid_apikey, array("turn_off_ssl_verification" => true));
+$email    = new SendGrid\Mail\Mail();
+$email->addTo($to);
+$email->setFrom($from, "問合せフォーム");
+$email->setSubject("[ContactForm] $subject");
+$email->addContent("text/plain", "Name: $name \r\nEmail: $emailadd \r\nSubject: $subject \r\nMessage: $message \r\n");
+$email->addContent("text/html", "<strong>Name:</strong> $name<br /> <strong>Email:</strong> $emailadd<br /> <strong>Subject:</strong> $subject<br /> <strong>Message:</strong> $message<br /> ");
+$email->addCategory('contact');
 
 $response = $sendgrid->send($email);
 //var_dump($response);
